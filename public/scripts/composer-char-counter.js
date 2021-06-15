98f843bed59e35dd37d3208fd66fb9d78e06fe12
Afzaan Hakim <afzaanhakim19@gmail.com>
2021-06-15 21:03:03 +0000

    implemented character counter to reduce based on text input and change color if it is lower than 0

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,28 @@
+
+$(document).ready(function () {
+
+
+const textArea = $('.new-tweet textarea');
+
+textArea.on('input', function(){
+  //console.log( event.target.value.length)
+  const inputLength = $(this).val().length
+  console.log(inputLength)
+  const charLeft = 140 - inputLength
+  console.log(charLeft)
+$('.counter').text(charLeft)
+
+$(this).find('.counter').text(charLeft)
+
+if (charLeft < 130){
+
+  $('.counter').addClass('limit-exceeded')
+
+}
+else {
+  $('.counter').removeClass('limit-exceeded')
+}
+
+})
+
+});
