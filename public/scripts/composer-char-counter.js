4fe3b447426d237e2b8aa14168d7be4787e68996
Afzaan Hakim <afzaanhakim19@gmail.com>
2021-06-15 21:04:47 +0000

    linting and reformatting

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,28 +1,20 @@
-
 $(document).ready(function () {
+  const textArea = $(".new-tweet textarea");
 
+  textArea.on("input", function () {
+    //console.log( event.target.value.length)
+    const inputLength = $(this).val().length;
+    console.log(inputLength);
+    const charLeft = 140 - inputLength;
+    console.log(charLeft);
+    $(".counter").text(charLeft);
 
-const textArea = $('.new-tweet textarea');
+    $(this).find(".counter").text(charLeft);
 
-textArea.on('input', function(){
-  //console.log( event.target.value.length)
-  const inputLength = $(this).val().length
-  console.log(inputLength)
-  const charLeft = 140 - inputLength
-  console.log(charLeft)
-$('.counter').text(charLeft)
-
-$(this).find('.counter').text(charLeft)
-
-if (charLeft < 130){
-
-  $('.counter').addClass('limit-exceeded')
-
-}
-else {
-  $('.counter').removeClass('limit-exceeded')
-}
-
-})
-
+    if (charLeft < 0) {
+      $(".counter").addClass("limit-exceeded");
+    } else {
+      $(".counter").removeClass("limit-exceeded");
+    }
+  });
 });
