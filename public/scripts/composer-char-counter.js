52421a8931977fe22b7d426c1aa6ccf67c76ee5c
Afzaan Hakim <afzaanhakim19@gmail.com>
2021-06-16 01:18:16 +0000

    implemented functionality for counting characters and reducing from character limit, also adding new class that changes counter color once beyond limit

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,20 +1,28 @@
+
 $(document).ready(function () {
-  const textArea = $(".new-tweet textarea");
 
-  textArea.on("input", function () {
-    //console.log( event.target.value.length)
-    const inputLength = $(this).val().length;
-    console.log(inputLength);
-    const charLeft = 140 - inputLength;
-    console.log(charLeft);
-    $(".counter").text(charLeft);
 
-    $(this).find(".counter").text(charLeft);
+const textArea = $('.new-tweet textarea');
 
-    if (charLeft < 0) {
-      $(".counter").addClass("limit-exceeded");
-    } else {
-      $(".counter").removeClass("limit-exceeded");
-    }
-  });
+textArea.on('input', function(){
+  //console.log( event.target.value.length)
+  const inputLength = $(this).val().length
+  console.log(inputLength)
+  const charLeft = 140 - inputLength
+  console.log(charLeft)
+$('.counter').text(charLeft)
+
+$(this).find('.counter').text(charLeft)
+
+if (charLeft < 0){
+
+  $('.counter').addClass('limit-exceeded')
+
+}
+else {
+  $('.counter').removeClass('limit-exceeded')
+}
+
+})
+
 });
