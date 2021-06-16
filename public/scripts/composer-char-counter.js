d6e5bd08fe1e53b348259aa60c32d2817e070941
Afzaan Hakim <afzaanhakim19@gmail.com>
2021-06-16 22:15:53 +0000

    redefined text-area element to adjust counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -5,11 +5,8 @@ $(document).ready(function () {
 const textArea = $('.new-tweet textarea');
 
 textArea.on('input', function(){
-  //console.log( event.target.value.length)
   const inputLength = $(this).val().length
-  console.log(inputLength)
   const charLeft = 140 - inputLength
-  console.log(charLeft)
 $('.counter').text(charLeft)
 
 $(this).find('.counter').text(charLeft)
