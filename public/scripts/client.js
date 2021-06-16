f7fce070ca9aff1a4ab62c4a84a7707a4c76dc76
Afzaan Hakim <afzaanhakim19@gmail.com>
2021-06-16 19:44:53 +0000

    implemented dynamic tweets, form submission once user clicks on tweet button and fetching database tweets using Ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,93 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
 
+
+
+const renderTweets = function(data) {
+  //empty the container
+
+  $('#tweet-container').empty()
+  for (let tweet of data){
+   let renVal = createTweetElement(tweet)
+   $('#tweet-container').append(renVal)
+  }
+}
+
+const createTweetElement = function(data) {
+
+  
+  // const $tweet = $('<article class ="tweet">')
+
+let htmlContent = 
+`<article class = "tweet">
+<header class = "user-header">
+<header class = "friend-profile"> <img class ="avatar" src="${data.user.avatars}"> ${data.user.name} </header>
+<header class = "friend-handle"> ${data.user.handle}</header>
+</header>
+<section class = "tweet-text"> ${data.content.text}</section>
+<footer class = "actions"><div class = "time"> ${timeago.format(data.created_at)} </div><div class = icons><i name = "retweet" class="fas fa-retweet"></i>
+<i name = "flag" class="fas fa-flag"></i>
+<i name = "heart" class="fas fa-heart"></i></div></footer>
+</article>`
+
+return htmlContent;
+}
+
+//const $tweet = createTweetElement(tweetData)
+
+// console.log($tweet); 
+// $('#tweets-container').append($tweet);
+
+
+
+
+
+
+
+const loadTweets = function() {
+const url = 'http://localhost:8080/tweets/'
+  $.ajax({
+  url,
+  method: 'GET'
+
+  })
+  .done((tweet)=> {
+
+    console.log('getting successful displayed')
+    renderTweets(tweet)
+  })
+  .fail(()=> {
+
+    console.log('could not load')
+  })
+
+
+}
+
+
+$(document).ready(function(){
+  loadTweets()
+  $('.form').on('submit', function(event){
+  
+    event.preventDefault()
+    console.log('clicekd on submit');
+  
+    $.ajax({
+      method: "POST",
+      url: 'http://localhost:8080/tweets',
+      data: $(this).serialize()
+  
+      })
+      .done(()=> {
+        console.log('successful posted')
+        $('textarea').val('');
+        
+      })
+      .fail(()=> {
+        console.log('posting failed')
+      })
+      .always(loadTweets())
+  })
+    
+  
+    
+  });
+//console.log(renderTweets(dataBase));
